5906782e2cf4c22ae0ba210270545edd1db1c830
Dylan Wyrick <xpoweroutage@gmail.com>
2023-11-28 14:08:47 -0500

    it now returns suggestions based on what is in the input

--- fastapi-frontend/src/pages/search/index.js
+++ fastapi-frontend/src/pages/search/index.js
@@ -1,47 +1,53 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const NutritionixApiExample = () => {
-  const [foodQuery, setFoodQuery] = useState('');
+  const [foodQuery, setFoodQuery] = useState("");
   const [error, setError] = useState(null);
 
-  const NUTRITIONIX_APP_ID = '0be43934';
-  const NUTRITIONIX_APP_KEY = '5622786a494b005ab83dee42b4282321';
-  const baseURL = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
+  const NUTRITIONIX_APP_ID = "0be43934";
+  const NUTRITIONIX_APP_KEY = "5622786a494b005ab83dee42b4282321";
+  const baseURL = "https://trackapi.nutritionix.com/v2/search/instant/";
 
   const handleSearch = async () => {
     try {
-      const response = await axios.post(baseURL, {
-        query: foodQuery,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'x-app-id': NUTRITIONIX_APP_ID,
-          'x-app-key': NUTRITIONIX_APP_KEY,
+      const response = await axios.post(
+        baseURL,
+        {
+          query: foodQuery,
         },
-      });
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-app-id": NUTRITIONIX_APP_ID,
+            "x-app-key": NUTRITIONIX_APP_KEY,
+          },
+        }
+      );
 
-      console.log('Nutrition Information:', response.data.foods);
+      console.log("response.data", response.data);
     } catch (error) {
-      setError('Error fetching data from Nutritionix API');
-      console.error('Error fetching data from Nutritionix API:', error);
+      setError("Error fetching data from Nutritionix API");
+      console.error("Error fetching data from Nutritionix API:", error);
     }
   };
 
   const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
+    setFoodQuery(e.target.value);
+    if (e.target.value.length > 3) {
       handleSearch();
     }
   };
 
+  //e.target.value > 4 then return result
+
   return (
-    <div>
+    <div className="container">
       <div>
         <input
           type="text"
           value={foodQuery}
-          onChange={(e) => setFoodQuery(e.target.value)}
-          onKeyPress={handleKeyPress} // Call handleKeyPress on key press
+          onChange={handleKeyPress}
           placeholder="Enter food query"
         />
         <button onClick={handleSearch}>Search</button>
