4e7fc045d049b85289a584f0b8066f98fe218b0e
Dylan Wyrick <xpoweroutage@gmail.com>
2023-12-02 13:35:25 -0500

    login_page

--- fastapi-frontend/src/pages/my_login/index.js
+++ fastapi-frontend/src/pages/my_login/index.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import { useGlobalState } from '../../context/GlobalState';
+import AuthService from '../../services/auth.service';
+import { jwtDecode } from "jwt-decode";
+import styles from './my_login.module.css'
+import Link from 'next/link';
 
 function LoginPage() {
-  return (
-    <>
-    <input placeholder='Email Address' />
-    <input placeholder='Password' />
-    <button onClick="function here" />
-    </>
-  )
-}
+    return (
+      <div className={styles.container}>
+        <div className={styles.formContainer}>
+          <div>
+            <p className={styles.inputText}>Email</p>
+            <input />
+          </div>
+          <div>
+            <p className={styles.inputText}>Password</p>
+            <input />
+          </div>
+          <button>Login</button>
+        </div>
+      </div>
+    );
+  }
+  
 
 export default LoginPage
