b6207b3f0e3649dad2f3248571e88c6a0e2ad207
w3schools-test <test@w3schools.com>
2023-12-02 21:51:48 -0500

    added functionality to login page

--- fastapi-frontend/src/pages/my_login/index.js
+++ fastapi-frontend/src/pages/my_login/index.js
@@ -5,20 +5,66 @@ import AuthService from '../../services/auth.service';
 import { jwtDecode } from "jwt-decode";
 import styles from './my_login.module.css'
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function LoginPage() {
+  const router = useRouter();
+    const { state, dispatch } = useGlobalState();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+//------------------------------------------------------------------------------------------------------------------------------
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const username = email;
+        AuthService
+            .login(username, password)
+            .then(async (resp) => {
+                if(resp != undefined){
+                    if (resp.access_token) {
+                        //let data = jwtDecode(resp.access_token);
+                        let data = jwtDecode(resp.access_token, { header: true });
+                        await dispatch({
+                            type: 'SET_USER',
+                            payload: data,
+                        });
+                        console.log('Login success');
+                        router.push('/');
+                    } else {
+                        console.log('Login failed');
+                        dispatch({ type: 'LOGOUT_USER' });
+                    }
+                }
+            })
+            .catch((error) => {
+                // Handle the error here
+                console.error('An error occurred:', error);
+                // Optionally, dispatch a logout or error action
+                dispatch({ type: 'LOGOUT_USER' });
+            })
+            .finally(() => {
+                // Code to run regardless of success or failure
+                console.log('Login request completed');
+            });
+    };
+
+
+
     return (
       <div className={styles.container}>
         <div className={styles.formContainer}>
+          <h1>Login</h1>
+          <form onSubmit={handleLogin}>
           <div>
-            <p className={styles.inputText}>Email</p>
-            <input />
+            <input  className={styles.inputEmail} placeholder='Email'onChange={(e) => setEmail(e.target.value)} required />
           </div>
           <div>
-            <p className={styles.inputText}>Password</p>
-            <input />
+            <input className={styles.inputPassword} placeholder='password' onChange={(e) => setPassword(e.target.value)} required minLength="8" />
           </div>
-          <button>Login</button>
+          
+          <button className={styles.loginButton}>Login</button>
+          </form>
+          <Link href="/register" className={styles.link}>Sign Up</Link>
         </div>
       </div>
     );
