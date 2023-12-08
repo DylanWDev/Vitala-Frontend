941bb692419ba3d50c9c1666831670a83e193705
Dylan Wyrick <xpoweroutage@gmail.com>
2023-12-07 19:25:07 -0500

    trying to get the data for the current logged in user in the dashboard file

--- fastapi-frontend/src/pages/dashboard/index.jsx
+++ fastapi-frontend/src/pages/dashboard/index.jsx
@@ -1,196 +1,113 @@
-import React from "react";
-import Link from "next/link";
-import styles from "./DashboardPage.module.css";
-import DataService from "@/services/data.service";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useGlobalState } from "@/context/GlobalState";
-import axios, { Axios } from "axios";
-import Nav from "@/components/Nav/Nav";
 import { jwtDecode } from "jwt-decode";
-
+import Nav from "@/components/Nav/Nav";
+import styles from "./DashboardPage.module.css";
 
 export default function DashboardPage() {
-  
-//   const [localState, setLocalState] =  useState({});
-//   const [globalState, dispatchEvent] = useGlobalState();
-
-//   useEffect( async () => {
-//     // Get Data and set State
-//     DataService
-//       .getData({
-//         data: "hello",
-//         headers: {
-//           "Content-Type": "application/x-www-form-urlencoded",
-//           "Authorization":"Bearer " + globalState.user.user_id
-//         }
-//       })
-//       .then(async (resp) => {
-//           if(resp != undefined){
-//               setLocalState(resp)
-//               await dispatchEvent({
-//                 type: 'GET_DATA',
-//                 payload: resp,
-//               });
-//           }
-//       })
-//       .catch((error) => {
-//           // Handle the error here
-//           console.error('An error occurred:', error);
-//       })
-//       .finally(() => {
-//           // Code to run regardless of success or failure
-//           console.log('Get Data request completed');
-//       });
-
-//       DataService
-//       .sendData({
-//         data: "something",
-//         headers: {
-//           "Content-Type": "application/x-www-form-urlencoded",
-//           "Authorization":"Bearer " + globalState.user.user_id
-//         }
-//       })
-//       .then(async (resp) => {
-//           if(resp != undefined){
-//               setLocalState(resp)
-//               await dispatchEvent({
-//                 type: 'SET_DATA_COMPLETED',
-//                 payload: resp,
-//               });
-//           }
-//       })
-//       .catch((error) => {
-//           // Handle the error here
-//           console.error('An error occurred:', error);
-//       })
-//       .finally(() => {
-//           // Code to run regardless of success or failure
-//           console.log('Set Data request completed');
-//       });
-//   }, [])
-  
-
-//!------------------------------------------------------------------
-
-  
-  let [data, setData] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [foodData, setFoodData] = useState(null);
+  const [foodQuery, setFoodQuery] = useState("");
+  const [error, setError] = useState(null);
+  const [searchResult, setSearchResult] = useState(null);
+  const [foods, setFoods] = useState([]);
+  const { state, dispatch } = useGlobalState();
 
   useEffect(() => {
-    let fetchData = async () => {
+    const getUserFromLocalStorage = () => {
+      const userData = localStorage.getItem('user');
+      if (userData) {
+        const user = jwtDecode(userData);
+        console.log('User data:', user);
+        dispatch({
+          type: 'SET_USER',
+          payload: user
+        });
+      }
+    };
+    getUserFromLocalStorage();
+
+    const fetchFoodData = async () => {
       try {
-        const response = await axios.get(
-          "http://127.0.0.1:8000/api/v1/foodlogs/all_food_logs"
-        );
-        setData(response.data);
-        console.log(response);
+        console.log('aaaaaaaaaaaaaaa')
+        const foodResponse = await axios.get("http://127.0.0.1:8000/api/v1/users/me");
+        console.log(foodResponse);
+        setFoodData(foodResponse.data);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching food data:", error);
       }
     };
 
-    fetchData();
+    fetchFoodData();
   }, []);
 
+  
   // Calculate total values
-  const totalProtein = Math.round((data ?? []).reduce((acc, entry) => acc + entry.protein, 0));
-  const totalCarbs = Math.round((data ?? []).reduce((acc, entry) => acc + entry.carbs, 0));
-  const totalFats = Math.round((data ?? []).reduce((acc, entry) => acc + entry.fats, 0));
+  const totalProtein = Math.round((foodData ?? []).reduce((acc, entry) => acc + entry.protein, 0));
+  const totalCarbs = Math.round((foodData ?? []).reduce((acc, entry) => acc + entry.carbs, 0));
+  const totalFats = Math.round((foodData ?? []).reduce((acc, entry) => acc + entry.fats, 0));
 
+  const NUTRITIONIX_APP_ID = "0be43934";
+  const NUTRITIONIX_APP_KEY = "5622786a494b005ab83dee42b4282321";
+  const nutritionixBaseURL = "https://trackapi.nutritionix.com/v2/natural/nutrients";
+  const backendBaseURL = "http://127.0.0.1:8000/api/v1/foodlogs/";
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.post(
+        nutritionixBaseURL,
+        {
+          query: foodQuery,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-app-id": NUTRITIONIX_APP_ID,
+            "x-app-key": NUTRITIONIX_APP_KEY,
+          },
+        }
+      );
 
-//!------------------------------------------------------------------
-
-
-
-const [foodQuery, setFoodQuery] = useState("");
-const [error, setError] = useState(null);
-const [searchResult, setSearchResult] = useState(null);
-const [foods, setFoods] = useState([]);
-
-const NUTRITIONIX_APP_ID = "0be43934";
-const NUTRITIONIX_APP_KEY = "5622786a494b005ab83dee42b4282321";
-const nutritionixBaseURL = "https://trackapi.nutritionix.com/v2/natural/nutrients";
-const backendBaseURL = "http://127.0.0.1:8000/api/v1/foodlogs/";
-
-
-const { state, dispatch} = useGlobalState();
-
-useEffect(() => {
-  const getUserFromLocalStorage = () => {
-    const userData = localStorage.getItem('user');
-    if (userData) {
-      const user = jwtDecode(userData);
-      console.log('User data:', user);
-      dispatch({
-        type: 'SET_USER',
-        payload: user
-      });
+      setSearchResult(response.data);
+      setFoods(response.data.foods || []);
+      console.log("Nutritionix Response:", response.data);
+    } catch (error) {
+      setError("Error fetching data from Nutritionix API");
+      console.error("Error fetching data from Nutritionix API:", error);
     }
   };
-  getUserFromLocalStorage();
-}, []);
 
-const fetchData = async () => {
-  try {
-    const response = await axios.post(
-      nutritionixBaseURL,
-      {
-        query: foodQuery,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "x-app-id": NUTRITIONIX_APP_ID,
-          "x-app-key": NUTRITIONIX_APP_KEY,
-        },
-      }
-    );
+  const handleClickEvent = async () => {
+    try {
+      const dataToSend = {
+        servings: foods[0]?.serving_qty || 0,
+        date_logged: foods[0]?.consumed_at || new Date().toISOString(),
+        calories: foods[0]?.nf_calories || 0.0,
+        protein: foods[0]?.nf_protein || 0.0,
+        carbs: foods[0]?.nf_total_carbohydrate || 0.0,
+        fats: foods[0]?.nf_total_fat || 0.0,
+        serving_unit: foods[0]?.serving_unit || "unit",
+        serving_weight_grams: foods[0]?.serving_weight_grams || 0.0,
+        meal_type: foods[0]?.meal_type || 0,
+        food_name: foods[0]?.food_name || "name",
+        user_id: parseInt(state.user.sub)
+      };
 
-    setSearchResult(response.data);
-    setFoods(response.data.foods || []); // Assuming the array of foods is in response.data.foods
-    console.log("Nutritionix Response:", response.data);
-  } catch (error) {
-    setError("Error fetching data from Nutritionix API");
-    console.error("Error fetching data from Nutritionix API:", error);
-  }
-};
+      const response = await axios.post(backendBaseURL, dataToSend);
 
-const handleClickEvent = async () => {
-  try {
-    const dataToSend = {
-      servings: foods[0]?.serving_qty || 0,
-      date_logged: foods[0]?.consumed_at || new Date().toISOString(),
-      calories: foods[0]?.nf_calories || 0.0,
-      protein: foods[0]?.nf_protein || 0.0,
-      carbs: foods[0]?.nf_total_carbohydrate || 0.0,
-      fats: foods[0]?.nf_total_fat || 0.0,
-      serving_unit: foods[0]?.serving_unit || "unit",
-      serving_weight_grams: foods[0]?.serving_weight_grams || 0.0,
-      meal_type: foods[0]?.meal_type || 0,
-      food_name: foods[0]?.food_name || "name",
-      user_id: parseInt(state.user.sub)
-    };
+      console.log('Backend Response:', response.data);
+    } catch (error) {
+      console.error('Error posting data to the backend:', error);
+    }
+  };
 
-    const response = await axios.post(backendBaseURL, dataToSend);
-
-    console.log('Backend Response:', response.data);
-  } catch (error) {
-    console.error('Error posting data to the backend:', error);
-  }
-};
-
-const handleKeyPress = (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    fetchData();
-  }
-};
-
-
-//!------------------------------------------------------------------
-
-
-
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      fetchData();
+    }
+  };
 
   return (
     <div className={styles.pageContainer}>
@@ -222,7 +139,6 @@ const handleKeyPress = (e) => {
               placeholder="Enter food query"
               className={styles.input}
             />
-            
           </div>
           {searchResult && (
             <div>
@@ -237,7 +153,6 @@ const handleKeyPress = (e) => {
           )}
         </div>
       </div>
-
     </div>
   );
 }
--- fastapi-frontend/src/pages/user_page/index.js
+++ fastapi-frontend/src/pages/user_page/index.js
@@ -5,11 +5,17 @@ import { jwtDecode } from "jwt-decode";
 
 function YourComponent() {
   const [age, setAge] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [gender, setGender] = useState("");
+  const [activityLevel, setActivityLevel] = useState(0);
+  const [healthGoals, setHealthGoals] = useState("");
+  
   const { state, dispatch } = useGlobalState();
 
-  const handleAgeChange = (event) => {
-    const inputAge = parseFloat(event.target.value);
-    setAge(isNaN(inputAge) ? 0 : inputAge);
+  const handleInputChange = (setter) => (event) => {
+    const inputValue = event.target.value;
+    setter(inputValue);
   };
 
   useEffect(() => {
@@ -27,28 +33,54 @@ function YourComponent() {
     getUserFromLocalStorage();
   }, []);
 
-  const handleClickEvent = async () => {
+  const handleUpdate = async (column, value) => {
     try {
       const requestBody = {
         accept: "application/json",
       };
 
       const response = await axios.put(
-        `http://127.0.0.1:8000/api/v1/users/data/${state.user.sub}?column_name=age&column_value=${age}`,
+        `http://127.0.0.1:8000/api/v1/users/data/${state.user.sub}?column_name=${column}&column_value=${value}`,
         requestBody
       );
 
-      console.log('Backend Response:', response.data);
+      console.log(`Backend Response for ${column}:`, response.data);
     } catch (error) {
-      console.error('Error posting data to the backend:', error);
+      console.error(`Error updating ${column}:`, error);
     }
   };
 
   return (
     <>
       <div>
-        <input value={age} onChange={handleAgeChange} />
-        <button onClick={handleClickEvent}>Submit</button>
+        <label>Age:</label>
+        <input value={age} onChange={handleInputChange(setAge)} />
+        <button onClick={() => handleUpdate('age', age)}>Update Age</button>
+      </div>
+      <div>
+        <label>Height:</label>
+        <input value={height} onChange={handleInputChange(setHeight)} />
+        <button onClick={() => handleUpdate('height', height)}>Update Height</button>
+      </div>
+      <div>
+        <label>Weight:</label>
+        <input value={weight} onChange={handleInputChange(setWeight)} />
+        <button onClick={() => handleUpdate('weight', weight)}>Update Weight</button>
+      </div>
+      <div>
+        <label>Gender:</label>
+        <input value={gender} onChange={handleInputChange(setGender)} />
+        <button onClick={() => handleUpdate('gender', gender)}>Update Gender</button>
+      </div>
+      <div>
+        <label>Activity Level:</label>
+        <input value={activityLevel} onChange={handleInputChange(setActivityLevel)} />
+        <button onClick={() => handleUpdate('activity_level', activityLevel)}>Update Activity Level</button>
+      </div>
+      <div>
+        <label>Health Goals:</label>
+        <input value={healthGoals} onChange={handleInputChange(setHealthGoals)} />
+        <button onClick={() => handleUpdate('health_goals', healthGoals)}>Update Health Goals</button>
       </div>
     </>
   );
